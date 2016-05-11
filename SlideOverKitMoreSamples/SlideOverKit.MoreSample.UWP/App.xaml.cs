87804b3737797f4d520a0026cc8299310da8a8cf
Jesse Jiang <jessejiang0214@outlook.com>
2016-05-11 11:08:28 +1000

    Fix UWP release dll loading issue

--- SlideOverKitMoreSamples/SlideOverKit.MoreSample.UWP/App.xaml.cs
+++ SlideOverKitMoreSamples/SlideOverKit.MoreSample.UWP/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
@@ -61,7 +62,15 @@ namespace SlideOverKit.MoreSample.UWP
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
-                Xamarin.Forms.Forms.Init(e);
+                // you'll need to add `using System.Reflection;`
+                List<Assembly> assembliesToInclude = new List<Assembly>();
+
+                //Now, add in all the assemblies your app uses
+                assembliesToInclude.Add(typeof(SlideOverKit.MenuContainerPage).GetTypeInfo().Assembly);
+                assembliesToInclude.Add(typeof(SlideOverKit.UWP.MenuContainerPageUWPRenderer).GetTypeInfo().Assembly);
+                //Also do this for all your other 3rd party libraries
+
+                Xamarin.Forms.Forms.Init(e, assembliesToInclude);
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
